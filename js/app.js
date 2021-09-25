640719ad5ed8e9a9a6c96bda432f61f1c7c6085e
giorgiovh <jorgevonhoroch@gmail.com>
2021-09-24 20:30:16 -0400

    adds state variables and chached elements

--- js/app.js
+++ js/app.js
@@ -2,7 +2,13 @@
 
 /*----- app's state (variables) -----*/ 
 
+let board = [], winner;
+
 /*----- cached element references -----*/ 
+
+const messageEl = document.querySelector('#message');
+const boardEl = document.querySelector('.board');
+const cardsNodeList = document.querySelectorAll('.card');
 
 /*----- event listeners -----*/ 
 
