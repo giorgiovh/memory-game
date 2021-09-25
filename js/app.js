5c7076b2f8cd9e4d56d7e5e9a2e04165c49f6507
giorgiovh <jorgevonhoroch@gmail.com>
2021-09-25 19:06:12 -0400

    adds matching pairs array

--- js/app.js
+++ js/app.js
@@ -2,43 +2,59 @@
 
 /*----- app's state (variables) -----*/ 
 
-let board = [], winner;
+let board = [], winStatus;
+let matchingPairsArr = [[0, 10], [10, 0], [1, 4], [4, 1], [2, 11], [11, 2], [3, 6], [6, 3], [5, 8], [8, 5], [7, 9], [9, 7]];
+let tempArr = [];
 
 /*----- cached element references -----*/ 
 
 const messageEl = document.querySelector('#message');
 const boardEl = document.querySelector('.board');
 const cardsNodeList = document.querySelectorAll('.card');
-const frontSideEls = document.querySelectorAll('.front-side');
-const backSideEls = document.querySelectorAll('.back-side');
+const frontSidesNodeList = document.querySelectorAll('.front-side');
+const backSidesNodeList = document.querySelectorAll('.back-side');
 
-console.log(backSideEls);
 /*----- event listeners -----*/ 
 
 boardEl.addEventListener('click', handleClick)
 
-/*----- functions -----*/
+/*----- init function -----*/
 
 init()
 
 function init() {
-    board = [null, null, null, null, null, null, null, null, null, null, null, null];
-    winner = null;
     messageEl.innerText = 'Make a move!'
+    board = [null, null, null, null, null, null, null, null, null, null, null, null];
+    winStatus = null;
     render();
 }
+
+/*----- on-click function -----*/
+
+function handleClick(event) {
+    let clickedEl = event.target;
+    let clickedElId = clickedEl.id;
+    board[clickedElId] = 1;
+    setWinStatus();
+    console.log(board);
+    render();
+}
+
+/*----- check winner function -----*/
+
+function setWinStatus() {
+    
+}
+/*----- render function -----*/
 
 function render() {
     board.forEach(function(card, i) {
         if (card === null) {
-            // show back of the card
+            frontSidesNodeList[i].setAttribute('hidden', true);
+        } else if (card === 1) {
+            backSidesNodeList[i].setAttribute('hidden', true);
+            frontSidesNodeList[i].removeAttribute('hidden')
         }
     })
 }
 
-function handleClick(event) {
-    let clickedEl = event.target;
-    let clickedElId = clickedEl.id
-    backSideEls[clickedElId].setAttribute('hidden', true);
-    frontSideEls[clickedElId].removeAttribute('hidden');
-}
